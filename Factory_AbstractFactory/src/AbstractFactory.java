74bceb6bae7c762afaeece742dfe4a1f1b0088d7
MaximKrasikov <karasy77@yandex.ru>
2018-08-21 11:51:02 +0300

    Update AbstractFactory.java

--- Factory_AbstractFactory/src/AbstractFactory.java
+++ Factory_AbstractFactory/src/AbstractFactory.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task37.task3702;
 
-public interface AbstractFactory { //наши фабрики Male & Femail реализуют этот интерфейс- фабрика фабрик
+public interface AbstractFactory { 
     public Human getPerson(int age);
 
 }
